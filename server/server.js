83bf4bd665b02e947c72913f544759734beb4487
Samuel Johnson <sj8285822@gmail.com>
2024-02-23 10:48:08 +0000

    dev Progress

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import 'dotenv/config';
 
 const app = express();
 let port = 3000;
 
-mongoose.connect('mongodb+srv://samcodes:<databnetwork>@cluster0.byqhoah.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect(process.env.DBOCATION, {
+    autoIndex: true,
+})
 
 app.listen(port, () =>{
     console.log("Server is listening on " + port);
