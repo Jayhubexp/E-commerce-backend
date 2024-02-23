bd121e5dcc1662d3aa66215256dfefe5e61d129b
Samuel Johnson <sj8285822@gmail.com>
2024-02-23 12:09:11 +0000

    Dev 1 added an api for post request to the server

--- server/server.js
+++ server/server.js
@@ -5,9 +5,20 @@ import 'dotenv/config';
 const app = express();
 let port = 3000;
 
+app.use(express.json());
+
 mongoose.connect(process.env.DBOCATION, {
     autoIndex: true,
 })
+
+app.post('/signup', (req, res) => {
+let {fullname, email, password} = req.body;
+   if(fullname.length < 3){
+      return res.status(403).json({error: "Name must be at least 3 characters"})
+      }
+   return res.status(200).json({message: "Okay"})
+})
+
 
 app.listen(port, () =>{
     console.log("Server is listening on " + port);
